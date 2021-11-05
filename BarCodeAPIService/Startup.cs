f482659a2541cb642e54cf700c30e6efa2d6a0bf
Smey <raksmeyprspan@gmail.com>
2021-11-05 16:06:25 +0700

    Master Data

--- BarCodeAPIService/Startup.cs
+++ BarCodeAPIService/Startup.cs
@@ -40,20 +40,7 @@ namespace BarCodeAPIService
             #region AddScope
             services.AddScoped<ITokenService, TokenService>();
             services.AddScoped<IGoodsReceiptPOService, GoodsReceiptPOService>();
-            services.AddScoped<IBPMasterDataService, BPMasterDataService>();
-            services.AddScoped<IWarehouseService, WarehouseService>();
-            services.AddScoped<IItemMasterDataService, ItemMasterDataService>();
-            services.AddScoped<IBinCodeService, BinCodeService>();
-            services.AddScoped<ICostCenterService, CostCenterService>();
-            services.AddScoped<IBatchNumberService, BatchNumberService>();
-            services.AddScoped<ISerialNumberService, SerialNumberService>();
-            services.AddScoped<IGLAccountService, GLAccountService>();
-            services.AddScoped<IContactPersonService, ContactPersonService>();
-            services.AddScoped<IBPAddressService, IBPAddressService>();
-            services.AddScoped<ISaleEmployeeService, SaleEmployeeService>();
-            services.AddScoped<IDocumentSeriesService, DocumentSeriesService>();
-            services.AddScoped<IPriceListService, PriceListService>();
-            services.AddScoped<IUserService, UserService>();
+
             #endregion
             #region ConfigureJWTToken
             var tokenvalidationParameters = new TokenValidationParameters
